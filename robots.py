edd8af9c673fd3444b66e95c780ead4202f3102d
zhl2008 <zhl2008vvvss@163.com>
2016-02-26 22:00:39 +0800

    第三次更新
    night的初步

--- robots.py
+++ robots.py
@@ -9,11 +9,8 @@ import sys
 
 
 #打印出pid由php负责存入数据库，可以在end时通过taskkill来结束进程
-sys.argv.append(open('tmp','r').readline())
 #config saved in config.py
-print "#langrensha_robots v 1.00"
-print "#copyright @haozi2016"
-print "\n"
+
 
 def err_log(msg):
     open('error.log','a+').write("%s : %s\n"%(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),msg))
@@ -58,14 +55,23 @@ class execute_sql():
         sql="insert into %s_role (qq,role) values ('%s','%s')"%(qq_group,qq,role)
         self.cursor.execute(sql)
         
-    def query_qq_by_id(self,uid,qq_group):
-        sql="select qq from %_perosonal where uid=%d"%(qq_group,uid)
+    def query_qq_by_id(self,uid):
+        sql="select qq from %s_perosonal where uid=%d"%(sys.argv[1],uid)
         self.cursor.execute(sql)
-                                                       
+
+    def query_qqs_by_role(self,role):
+        sql="select qq from %s_role where role=%s"%(sys.argv[1],role)
+        self.cursor.execute(sql)
+    
+    def role_is_alive_or_exist(self,role):
+        sql="select qq form %s_role where role=%s and is_alive=1"%(sys.argv[1],role)
+        self.cursor.execute(sql)
+
+
+
     def mysql_error(self):
         pass
-        
-
+    
     def __del__(self):
         self.cursor.close()
 
@@ -99,11 +105,54 @@ class message():
     def insert_role(self,qq,qq_group,role):
         self.mysql.insert_role(qq,qq_group,role)
         
-        
+    def send_return_data(self,qq,method,return_data):
+        con=httplib.HTTPConnection("%s"%config.host,config.listen_port,timeout=5)
+        con.request("GET","/?key=%s&a=%s"%(cofig.key,self.serialize_data(qq,method,return_data)))
+        con.close()  
+
 
 class night():
     def __init__(self):
-        pass
+        self.is_first_night=1
+
+    def start(self,a):
+        a.send_return_data(qq_group,"SendClusterMessage","the night is comming!")
+        self.fisrt_night(a)
+
+    def fisrt_night(self,a):
+        #第一夜
+        if(self.is_first_night):
+            if(role_is_alive_or_exist('love_god')){
+                a.write_return_data(role_is_alive_or_exist('love_god'),"SendMessage","please choose the number of the player you want to connect")
+                #爱神的判断逻辑        
+            }
+            if(role_is_alive_or_exist('')):
+                pass
+            is_first_night=0
+        #其他的夜晚   
+        self.wolf_kill(a)
+    def wolf_kill(self,a):   
+        for wolf in range(self.role_is_alive_or_exist("wolf")):
+            a.send_return_data(wolf,"SendMessage","please choose a people to kill,just send the number of that people,and you only have 40s to decide")
+        [is_qq_group,qq,message,qq_group,nickname]=a.get_message()
+        vote_array={}
+        now_time=time.time()
+        while (len(vote_array)<len(role_is_alive_or_exist("wolf")) or check_vote(vote_array) or(time.time()-now_time)>40):
+            if not is_qq_group and message.isdigit():
+                vote_array[qq]=int(message)
+                return_data=""
+                for vote_wolf in vote_array:
+                    return_data+="wolf %s has vote to kill %d\n"%(vote_wolf,vote_array[vote_wolf])
+                for wolf in self.role_is_alive_or_exist("wolf"):
+                     a.send_return_data(wolf,"SendMessage",return_data+"you can vote again to change your choice")
+            [is_qq_group,qq,message,qq_group,nickname]=a.get_message()
+        if(check_vote(vote_array)):
+            
+
+
+            
+
+
 
 class day():
     def __init__(self):
@@ -117,22 +166,21 @@ def is_authority(qq):
     if qq==config.admin_qq:
         return 1
     else:
-        return 0
-    
+        return 0    
     
 #准备游戏，先统计游戏人数
 def prepare():
     global a,b,c
     player=0
     prepare=0
-    data=a.get_message()
-    print data
-    while data[0][0]==1:
-        if (data[0][2]=="join" and not a.check_exist_player(data[0][1]) and not prepare):
+    [is_qq_group,qq,message,qq_group,nickname]=a.get_message()
+
+    while is_qq_group==1:
+        if (message=="join" and not a.check_exist_player(qq) and not prepare):
             player+=1
-            a.insert_player(data[0][1],data[0][3],data[0][4])
-            a.write_return_data(data[0][3],"SendClusterMessage","player %d %s has joined the game "%(player,data[0][4]))
-        elif data[0][2]=="prepare" and is_authority(data[0][1]):
+            a.insert_player(qq,qq_group,nickname)
+            a.write_return_data(qq_group,"SendClusterMessage","player %d %s has joined the game "%(player,nickname))
+        elif message=="prepare" and is_authority(qq):
             if config.config_array.has_key(player):
                 prepare=1
                 return_data="you can choose the config below:\n"
@@ -140,27 +188,33 @@ def prepare():
                 for choice_array in  config.config_array[player]:
                     return_data=return_data+str(i)+' '+','.join(choice_array)+'\n'
                     i+=1
-                a.write_return_data(data[0][3],"SendClusterMessage",return_data)
+                a.write_return_data(qq_group,"SendClusterMessage",return_data)
             else:
-                a.write_return_data(data[0][3],"SendClusterMessage","player number does not match any of your config!")
+                a.write_return_data(qq_group,"SendClusterMessage","player number does not match any of your config!")
            
-        elif data[0][2].isdigit() and prepare and is_authority(data[0][1]):
-            if int(data[0][2])<len(config.config_array[player]):
-                my_array=config.config_array[player][int(data[0][2])]
-                a.write_return_data(data[0][3],"SendClusterMessage","your game config is %s"%(','.join(my_array)))
+        elif message.isdigit() and prepare and is_authority(qq):
+            if int(message)<len(config.config_array[player]):
+                my_array=config.config_array[player][int(message)]
                 random.shuffle(my_array)
+                a.write_return_data(qq_group,"SendClusterMessage","your game config is %s"%(','.join(my_array))+return_data)
                 for i in range(player):
-                    a.insert_role(data[0][1],data[0][3],my_array[i])
-                    a.write_return_data(a.mysql.query_qq_by_uid(i,data[0][3])[0][0],"SendMessage","your role is %s"%my_array[i])
-                    
+                    a.insert_role(qq,qq_group,my_array[i])
+                    a.send_return_data(a.mysql.query_qq_by_uid(i,qq_group)[0][0],"SendMessage","your role is %s"%my_array[i])
+                b.start(a)
+                
+                #在调用相关的night函数之前需要
         else:
-            a.write_return_data(data[0][3],"SendClusterMessage","noreply")
+            a.write_return_data(qq_group,"SendClusterMessage","noreply")
             
-        data=a.get_message()
+        [is_qq_group,qq,message,qq_group,nickname]=a.get_message()
         
     
             
 if __name__ == "__main__":
+    sys.argv.append(open('tmp','r').readline())
+    print "#langrensha_robots v 1.00"
+    print "#copyright @haozi2016"
+    print "\n"
     global a,b,c
     a=message()
     b=night()
